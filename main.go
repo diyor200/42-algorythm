bfb72064df400f3a2720313f678a262632bfe8d1
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-12 22:09:01 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+type Solution struct{}
+
+func (Solution) ReverseString(s []string) []string {
+	i := 0
+	j := len(s) - 1
+
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+
+		i++
+		j--
+	}
+
+	return s
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestSolution(t *testing.T) {
+	s := []string{"h", "e", "l", "l", "o"}
+
+	sol := Solution{}
+	t.Log(sol.ReverseString(s))
+}
