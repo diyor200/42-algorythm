51a8baaf519b11df2643c5e799e5d0371c7b1043
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-14 21:36:12 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,57 @@
 package main
 
 type Solution struct{}
+
+const (
+	open  = '('
+	close = ')'
+)
+
+func (Solution) Is_valid(s string) bool {
+	balance := 0
+
+	for i := range s {
+		if s[i] == open {
+			balance++
+		} else if s[i] == close {
+			balance--
+		}
+
+		if balance < 0 {
+			return false
+		}
+	}
+
+	return balance == 0
+}
+
+// leetcode: https://leetcode.com/problems/valid-parentheses/description/
+
+func (Solution) isValid(s string) bool {
+	n := len(s)
+	if n%2 != 0 {
+		return false
+	}
+
+	stack := make([]rune, n)
+	top := -1
+	pairs := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, ch := range s {
+		if ch == '(' || ch == '{' || ch == '[' {
+			top++
+			stack[top] = ch
+		} else {
+			if top == -1 || stack[top] != pairs[ch] {
+				return false
+			}
+			top-- // pop
+		}
+	}
+
+	return top == -1
+}
--- main_test.go
+++ main_test.go
@@ -3,7 +3,10 @@ package main
 import "testing"
 
 func TestSolution(t *testing.T) {
-
+	// input := "((()))"
 	s := Solution{}
-	t.Log(s)
+	// t.Log(s.Is_valid(input))
+
+	input1 := "([)"
+	t.Log(s.isValid(input1))
 }
