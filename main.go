e7f7087d7c85764ee4bf3d9590bfa8dbb76406d5
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-14 22:22:45 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
 type Solution struct{}
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+func (Solution) MiddleNode(head *Node) *Node {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
--- main_test.go
+++ main_test.go
@@ -1,9 +1,37 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSolution(t *testing.T) {
-
 	s := Solution{}
-	t.Log(s)
+
+	l := []int{1, 2, 3, 4, 5}
+	head := createLinkedList(l)
+
+	middle := s.MiddleNode(head)
+	t.Log(middle)
+	read(middle)
+}
+
+func createLinkedList(list []int) *Node {
+	head := &Node{Val: list[0]}
+	current := head
+
+	for _, v := range list[1:] {
+		current.Next = &Node{Val: v}
+		current = current.Next
+	}
+
+	return head
+
+}
+
+func read(head *Node) {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
 }
