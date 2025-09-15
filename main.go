57875e75d87ddbe33ee7a6d680888a791b67e08c
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-15 22:58:15 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,53 @@
 package main
 
 type Solution struct{}
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+func findMiddle(head *Node) *Node {
+	fast, slow := head, head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
+
+func reverse(head *Node) *Node {
+	var prev *Node
+	curr := head
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	return prev
+}
+
+func isSame(head1, head2 *Node) bool {
+	for head1 != nil && head2 != nil {
+		if head1.Val != head2.Val {
+			return false
+		}
+
+		head1 = head1.Next
+		head2 = head2.Next
+	}
+
+	return true
+}
+
+func (Solution) IsPalindrome(head *Node) bool {
+	middle := findMiddle(head)
+	middle = reverse(middle)
+
+	return isSame(head, middle)
+}
--- main_test.go
+++ main_test.go
@@ -9,6 +9,30 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
+	arr := []int{1, 2, 2, 1}
+	assert.Equal(t, true, s.IsPalindrome(create(arr)))
 	t.Log("pass")
+}
+
+func create(arr []int) *Node {
+	head := &Node{Val: arr[0]}
+	curr := head
+
+	for _, v := range arr[1:] {
+		curr.Next = &Node{Val: v}
+		curr = curr.Next
+	}
+
+	return head
+}
+
+func read(head *Node) []int {
+	var res []int
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
 }
