b58cb5e28a2af5463cde7fc772ac1b7c60393a11
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-15 23:22:22 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
 package main
 
 type Solution struct{}
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+func (Solution) RemoveNthFromEnd(head *Node, n int) *Node {
+	node := &Node{Next: head}
+	fast, slow := node, node
+
+	for i := 0; i <= n; i++ {
+		fast = fast.Next
+	}
+
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return node.Next
+}
--- main_test.go
+++ main_test.go
@@ -9,6 +9,36 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
+	arr1 := []int{1, 2, 3, 4, 5}
+	n1 := 2
+	assert.Equal(t, []int{1, 2, 3, 5}, read(s.RemoveNthFromEnd(create(arr1), n1)))
+
+	arr2 := []int{1}
+	n2 := 1
+	assert.Equal(t, nil, read(s.RemoveNthFromEnd(create(arr2), n2)))
+
 	t.Log("pass")
+}
+
+func create(arr []int) *Node {
+	head := &Node{Val: arr[0]}
+	curr := head
+
+	for _, val := range arr[1:] {
+		curr.Next = &Node{Val: val}
+		curr = curr.Next
+	}
+
+	return head
+}
+
+func read(head *Node) []int {
+	var res []int
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
 }
