523fdbebc4baefe9a7bf9664901b7b90cdbf890c
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-15 22:06:27 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
 type Solution struct{}
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+func (Solution) Reverse(head *Node) *Node {
+	var prev *Node
+	curr := head
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	return prev
+}
--- main_test.go
+++ main_test.go
@@ -1,9 +1,39 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSolution(t *testing.T) {
-
 	s := Solution{}
-	t.Log(s)
+
+	arr := []int{1, 2, 3, 4, 5}
+	head := s.Reverse(createLinkedList(arr))
+
+	assert.Equal(t, []int{5, 4, 3, 2, 1}, read(head))
+	t.Log("pass")
+}
+
+func createLinkedList(arr []int) *Node {
+	head := &Node{Val: arr[0]}
+	current := head
+
+	for _, v := range arr[1:] {
+		current.Next = &Node{Val: v}
+		current = current.Next
+	}
+
+	return head
+}
+
+func read(head *Node) []int {
+	var res []int
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
 }
