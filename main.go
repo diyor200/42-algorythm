4929c2c22f9278e91382563eae1efd1012b60a9f
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-16 22:57:48 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
 package main
 
 type Solution struct{}
+
+func (Solution) DetectCapitalUse(word string) bool {
+	if isUpper(word) || isLower(word) {
+		return true
+	}
+
+	return isUpper(word[:1]) && isLower(word[1:])
+}
+
+func isUpper(s string) bool {
+	return inRange(s, 'A', 'Z')
+}
+func isLower(s string) bool { return inRange(s, 'a', 'z') }
+
+func inRange(s string, start, end rune) bool {
+	for _, ch := range s {
+		if !(ch >= start && ch <= end) {
+			return false
+		}
+	}
+
+	return true
+}
--- main_test.go
+++ main_test.go
@@ -9,6 +9,22 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
-	t.Log("pass")
+	cases := []struct {
+		Name     string
+		Word     string
+		Expected bool
+	}{
+		{Word: "USA", Expected: true, Name: "true case 1"},
+		{Word: "FlaG", Expected: false, Name: "wrong case 1"},
+		{Word: "Name", Expected: true, Name: "true case 2"},
+		{Word: "TeST", Expected: false, Name: "wrong case 2"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			assert.Equal(t, c.Expected, s.DetectCapitalUse(c.Word))
+		})
+	}
+
+	t.Log("pass!")
 }
