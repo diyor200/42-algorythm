bd3a1837a7bc44c2fa46ab3d57e0bcf49f813dd8
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-25 21:30:08 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
 type Solution struct{}
+
+func (Solution) ContainsDuplicate(nums []int) bool {
+	var m = make(map[int]struct{}, len(nums))
+	for i := range nums {
+		if _, ok := m[nums[i]]; ok {
+			return true
+		}
+
+		m[nums[i]] = struct{}{}
+	}
+
+	return false
+}
--- main_test.go
+++ main_test.go
@@ -9,6 +9,21 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
+	testCases := []struct {
+		name string
+		arr  []int
+		want bool
+	}{
+		{name: "first", arr: []int{1, 2, 3, 1}, want: true},
+		{name: "second", arr: []int{1, 2, 3, 4}, want: false},
+		{name: "third", arr: []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, want: true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, s.ContainsDuplicate(tt.arr))
+		})
+	}
+
 	t.Log("pass")
 }
