54f19f49d4f8e90b33b422bc31cd3743ce9396b1
Diyorbek <Diyorbek_Abdulaxatov>
2025-09-25 16:41:31 +0500

    max area issue solved

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
 type Solution struct{}
+
+func (Solution) MaxArea(nums []int) int {
+	i, j := 0, len(nums)-1
+	result := 0
+
+	for i < j {
+		h := 0
+		if nums[i] < nums[j] {
+			h = nums[i]
+			i++
+		} else {
+			h = nums[j]
+			j--
+		}
+		area := (j - i + 1) * h // +1 bo‘lishi shart emas, lekin to‘g‘ri joyda kamaytirilganiga qarab ishlatiladi
+		if area > result {
+			result = area
+		}
+	}
+
+	return result
+}
--- main_test.go
+++ main_test.go
@@ -9,6 +9,20 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
+	cases := []struct {
+		name     string
+		arr      []int
+		expected int
+	}{
+		{name: "first", arr: []int{1, 8, 6, 2, 5, 4, 8, 3, 7}, expected: 49},
+		{name: "second", arr: []int{1, 1}, expected: 1},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			assert.Equal(t, c.expected, s.MaxArea(c.arr))
+		})
+	}
+
 	t.Log("pass")
 }
