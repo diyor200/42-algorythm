46d742cadcd8c5d0f70435ec1a139917c91b3338
Diyorbek <Diyorbek_Abdulaxatov>
2025-09-25 17:27:00 +0500

    arqon tortish issue id solved

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
 type Solution struct{}
+
+func (Solution) LeftRightDifference(nums []int) []int {
+	leftSum := 0
+	rightSum := 0
+	result := make([]int, len(nums))
+	for i := range nums {
+		if i == 0 {
+			rightSum = sum(nums) - nums[i]
+		} else {
+			leftSum += nums[i-1]
+			rightSum -= nums[i]
+		}
+
+		if leftSum > rightSum {
+			result[i] = -1
+		} else if leftSum < rightSum {
+			result[i] = 1
+		} else {
+			result[i] = 0
+		}
+	}
+
+	return result
+}
+
+func sum(nums []int) int {
+	result := 0
+	for i := range nums {
+		result += nums[i]
+	}
+
+	return result
+}
--- main_test.go
+++ main_test.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"testing"
-
 	"github.com/go-playground/assert/v2"
+	"testing"
 )
 
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
+	cases := []struct {
+		name string
+		arr  []int
+		want []int
+	}{
+		{name: "first", arr: []int{1, 2, 3, 4}, want: []int{1, 1, 1, -1}},
+		{name: "second", arr: []int{10, 4, 8, 3}, want: []int{1, 1, -1, -1}},
+		{name: "third", arr: []int{1, 2, 3, 4, 5}, want: []int{1, 1, 1, -1, -1}},
+		{name: "four", arr: []int{0, 0, 0, 0, 0}, want: []int{0, 0, 0, 0, 0}},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, s.LeftRightDifference(tt.arr))
+		})
+	}
+
 	t.Log("pass")
 }
