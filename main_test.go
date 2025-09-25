400233dc8325f7547d20709ab6114af0d3adca0e
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-25 21:32:34 +0500

    v2.0

--- main_test.go
+++ main_test.go
@@ -9,6 +9,18 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
-	assert.Equal(t, s, s)
+	testCases := []struct {
+		name string
+		want int
+	}{
+		{name: "test", want: 0},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, s)
+		})
+	}
+
 	t.Log("pass")
 }
