421291464aed1e5cdec84d8a3d3d7d2f2dcdb16a
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-26 10:55:49 +0500

    begun solving two sum problem

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
 type Solution struct{}
+
+func (Solution) TwoSum(nums []int, target int) []int {
+	set := make(map[int]struct{}, len(nums))
+	res := make([]int, 2)
+
+	for i := range nums {
+		if _, ok := set[nums[i]]; ok {
+			continue
+		}
+
+		set[nums[i]] = struct{}{}
+	}
+
+	return res
+}
