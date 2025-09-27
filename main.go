0f88bd2756b1431eae36a7fec27ce55770ad54ac
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-27 21:54:55 +0500

    bad solution. Need to improve

--- main.go
+++ main.go
@@ -1,3 +1,56 @@
 package main
 
+import "strconv"
+
 type Solution struct{}
+
+func (Solution) GroupAnagrams(words []string) int {
+	var groups = make(map[byte]int8)
+
+	for i := range words {
+		var key byte
+		for j := range words[i] {
+			key += words[i][j]
+		}
+
+		groups[key] += 1
+	}
+
+	var res int
+	for range groups {
+		res++
+	}
+
+	return res
+}
+
+// for leetcode: https://leetcode.com/problems/group-anagrams/
+func groupAnagrams(strs []string) [][]string {
+	if len(strs) == 1 {
+		return [][]string{strs}
+	}
+
+	var groups = make(map[string][]string)
+	for i := range strs {
+		letters := [26]int16{}
+		for j := range strs[i] {
+			letters[strs[i][j]-'a']++
+		}
+
+		var key string
+		for l := range letters {
+			if letters[l] > 0 {
+				key += string('a'+l) + strconv.Itoa(int(letters[l]))
+			}
+		}
+
+		groups[key] = append(groups[key], strs[i])
+	}
+
+	var res = make([][]string, 0, len(groups))
+	for k := range groups {
+		res = append(res, groups[k])
+	}
+
+	return res
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,35 @@ func TestSolution(t *testing.T) {
 
 	testCases := []struct {
 		name string
+		arr  []string
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "first", arr: []string{"eat", "tea", "tan", "ate", "nat", "bat"}, want: 3},
+		{name: "second", arr: []string{"a"}, want: 1},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.GroupAnagrams(tt.arr))
+		})
+	}
+
+	t.Log("pass")
+}
+
+func TestAngramGroups(t *testing.T) {
+	testCases := []struct {
+		name string
+		arr  []string
+		want [][]string
+	}{
+		{name: "first", arr: []string{"eat", "tea", "tan", "ate", "nat", "bat"}, want: [][]string{{"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}}},
+		{name: "second", arr: []string{""}, want: [][]string{{""}}},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, groupAnagrams(tt.arr))
 		})
 	}
 
