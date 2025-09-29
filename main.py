5a507cc1df133aa36a5806c9ff07d7660c2bc401
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-29 21:04:55 +0500

    solution

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-r = [1,2,3,4,5]
-print(r[:len(r)-2])
-class MyStack:
+class MyQueue:
 
     def __init__(self):
-        self.data = list()
+        self.data = []
         self.l = 0
-        
 
     def push(self, x: int) -> None:
         self.data.append(x)
         self.l += 1
-        return
 
     def pop(self) -> int:
-        if self.l == 0:
+        if self.empty():
             return None
-        
-        res = self.data[self.l-1]
-        self.data = self.data[:self.l-2]
-        self.l -= 1
 
+        res = self.data[0]
+        self.data = self.data[1:]
+        self.l -= 1
         return res
 
-    def top(self) -> int:
-        if self.l == 0:
-            return None
-        
-        return self.data[self.l-1]
-        
+    def peek(self) -> int:
+      if self.empty():
+        return None
+      return self.data[0]
 
     def empty(self) -> bool:
         return self.l == 0
