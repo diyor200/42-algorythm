5640d2643af8168a4c9c4d09f326e8b90e788908
Diyorbek <Diyorbek_Abdulaxatov>
2025-09-29 11:35:30 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
 type Solution struct{}
+
+func (Solution) Generate(n int) [][]int {
+	if n == 0 {
+		return [][]int{}
+	}
+
+	row := []int{1}
+	res := make([][]int, n)
+	res[0] = row
+
+	for i := 1; i < n; i++ {
+		row = generateRow(row)
+		res[i] = row
+	}
+
+	return res
+}
+
+func generateRow(prev []int) []int {
+	row := make([]int, len(prev)+1)
+	row[0] = 1
+	row[len(row)-1] = 1
+	for i := 0; i < len(prev)-1; i++ {
+		row[i+1] = prev[i] + prev[i+1]
+	}
+
+	return row
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,16 @@ func TestSolution(t *testing.T) {
 
 	testCases := []struct {
 		name string
-		want int
+		n    int
+		want [][]int
 	}{
-		{name: "test", want: 0},
+		{name: "first", n: 2, want: [][]int{{1}, {1, 1}}},
+		{name: "second", n: 5, want: [][]int{{1}, {1, 1}, {1, 2, 1}, {1, 3, 3, 1}, {1, 4, 6, 4, 1}}},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.Generate(tt.n))
 		})
 	}
 
