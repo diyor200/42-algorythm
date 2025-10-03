58da4c5c296450254fd63c4f3da7e6ae3e7ff34b
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-04 00:14:35 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,51 @@
 package main
 
 type Solution struct{}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (Solution) Count_leaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var stack []*TreeNode
+	var visited = make(map[*TreeNode]struct{})
+	var res int
+
+	stack = append(stack, root)
+
+	for len(stack) != 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if _, ok := visited[node]; !ok {
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+			if node.Right == nil && node.Left == nil {
+				res++
+			}
+			visited[node] = struct{}{}
+		}
+	}
+
+	return res
+}
+
+// func (s Solution) Count_leaves(root *TreeNode) int {
+// 	if root == nil {
+// 		return 0
+// 	}
+// 	if root.Left == nil && root.Right == nil {
+// 		return 1
+// 	}
+
+// 	return s.Count_leaves(root.Left) + s.Count_leaves(root.Right)
+// }
--- main_test.go
+++ main_test.go
@@ -8,17 +8,26 @@ import (
 
 func TestSolution(t *testing.T) {
 	s := Solution{}
+	root1 := &TreeNode{Val: 2}
+	root1.Left = &TreeNode{Val: 1}
+	root1.Right = &TreeNode{Val: 3}
+
+	root2 := &TreeNode{Val: 5}
+	root2.Left = &TreeNode{Val: 1}
+	root2.Right = &TreeNode{Val: 4, Right: &TreeNode{Val: 6}, Left: &TreeNode{Val: 3}}
 
 	testCases := []struct {
 		name string
+		root *TreeNode
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "first", root: root1, want: 2},
+		{name: "second", root: root2, want: 3},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.Count_leaves(tt.root))
 		})
 	}
 
