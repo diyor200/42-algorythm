3934c2593fff03d4ee3df01eb049fdfe09acea08
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-09 21:24:26 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,39 @@
 package main
 
 type Solution struct{}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// diameter = radius + radius
+func (s Solution) DiameterOfBinaryTree(root *TreeNode) int {
+	maxDiameter := 0
+	findDepth(root, &maxDiameter)
+	return maxDiameter
+}
+
+func findDepth(root *TreeNode, maxDiameter *int) int {
+	if root == nil {
+		return 0
+	}
+
+	left := findDepth(root.Left, maxDiameter)
+	right := findDepth(root.Right, maxDiameter)
+
+	// Update the maximum diameter if current path is longer
+	if left+right > *maxDiameter {
+		*maxDiameter = left + right
+	}
+
+	return max(left, right) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- main_test.go
+++ main_test.go
@@ -9,16 +9,27 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  &TreeNode{Val: 4},
+			Right: &TreeNode{Val: 5},
+		},
+		Right: &TreeNode{Val: 3},
+	}
+
 	testCases := []struct {
 		name string
+		root *TreeNode
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "test", root: root, want: 3},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.DiameterOfBinaryTree(tt.root))
 		})
 	}
 
