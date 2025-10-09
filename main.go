7fc86e90d871a4e8a80d1bff80c23b3fbf24be79
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-09 20:48:31 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,96 @@
 package main
 
 type Solution struct{}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// steps:
+// 1. invert right subtree of root
+// 2. compare with left subtree
+// func (s Solution) IsSymmetric(root *TreeNode) bool {
+// 	if root == nil {
+// 		return true
+// 	}
+
+// 	var queue = []*TreeNode{root.Left, root.Right}
+// 	for len(queue) > 0 {
+// 		left := queue[0]
+// 		right := queue[1]
+// 		queue = queue[2:]
+
+// 		if left == nil && right == nil {
+// 			continue
+// 		}
+
+// 		if left == nil || right == nil {
+// 			return false
+// 		}
+
+// 		if left.Val != right.Val {
+// 			return false
+// 		}
+
+// 		queue = append(queue, left.Left, right.Right)
+// 		queue = append(queue, left.Right, right.Left)
+// 	}
+
+// 	return true
+// }
+
+// func (s Solution) IsSymmetric(root *TreeNode) bool {
+// 	if root == nil {
+// 		return true
+// 	}
+
+// 	queue := []*TreeNode{root.Left, root.Right}
+
+// 	for len(queue) > 0 {
+// 		left := queue[0]
+// 		right := queue[1]
+// 		queue = queue[2:]
+
+// 		if left == nil && right == nil {
+// 			continue
+// 		}
+// 		if left == nil || right == nil {
+// 			return false
+// 		}
+// 		if left.Val != right.Val {
+// 			return false
+// 		}
+
+// 		// Add mirrored children
+// 		queue = append(queue, left.Left, right.Right)
+// 		queue = append(queue, left.Right, right.Left)
+// 	}
+
+// 	return true
+// }
+
+func (s Solution) IsSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isEqual(root.Left, root.Right)
+}
+
+func isEqual(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isEqual(left.Left, right.Right) && isEqual(left.Right, right.Left)
+}
--- main_test.go
+++ main_test.go
@@ -9,16 +9,30 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
+	root1 := &TreeNode{Val: 1}
+	left1 := &TreeNode{Val: 2, Right: &TreeNode{Val: 4}, Left: &TreeNode{Val: 3}}
+	right1 := &TreeNode{Val: 2, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 3}}
+	root1.Left = left1
+	root1.Right = right1
+
+	root2 := &TreeNode{Val: 1}
+	left2 := &TreeNode{Val: 2, Right: &TreeNode{Val: 3}}
+	right2 := &TreeNode{Val: 2, Right: &TreeNode{Val: 3}}
+	root2.Left = left2
+	root2.Right = right2
+
 	testCases := []struct {
 		name string
-		want int
+		root *TreeNode
+		want bool
 	}{
-		{name: "test", want: 0},
+		{name: "first", root: root1, want: true},
+		{name: "second", root: root2, want: false},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.IsSymmetric(tt.root))
 		})
 	}
 
