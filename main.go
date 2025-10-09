7d6a8173fec88a61657045de180aeecce991ce04
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-09 23:26:03 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
 type Solution struct{}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (s Solution) LevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	res := [][]int{}
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		layer := []int{}
+		temp := []*TreeNode{}
+		for i := range queue {
+			layer = append(layer, queue[i].Val)
+			if queue[i].Left != nil {
+				temp = append(temp, queue[i].Left)
+			}
+			if queue[i].Right != nil {
+				temp = append(temp, queue[i].Right)
+			}
+		}
+		res = append(res, layer)
+		queue = temp
+	}
+
+	return res
+}
--- main_test.go
+++ main_test.go
@@ -9,16 +9,27 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
+	root := &TreeNode{
+		Val:  3,
+		Left: &TreeNode{Val: 9},
+		Right: &TreeNode{
+			Val:   20,
+			Left:  &TreeNode{Val: 15},
+			Right: &TreeNode{Val: 7},
+		},
+	}
+
 	testCases := []struct {
 		name string
-		want int
+		root *TreeNode
+		want [][]int
 	}{
-		{name: "test", want: 0},
+		{name: "first", root: root, want: [][]int{{3}, {9, 20}, {15, 7}}},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.LevelOrder(tt.root))
 		})
 	}
 
