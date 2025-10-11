6d691f4b479a9d056bb0678e932502fcb58b713a
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-11 22:46:21 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package main
 
 type Solution struct{}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (s Solution) SumNumbers(root *TreeNode) int {
+	return dfs(root, 0)
+}
+
+func dfs(root *TreeNode, cur int) int {
+	if root == nil {
+		return 0
+	}
+
+	cur = cur*10 + root.Val
+
+	if root.Left == nil && root.Right == nil {
+		return cur
+	}
+
+	return dfs(root.Left, cur) + dfs(root.Right, cur)
+}
--- main_test.go
+++ main_test.go
@@ -9,16 +9,24 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
+	root := &TreeNode{Val: 1,
+		Left: &TreeNode{Val: 2,
+			Left:  &TreeNode{Val: 7},
+			Right: &TreeNode{Val: 9},
+		},
+		Right: &TreeNode{Val: 9}}
+
 	testCases := []struct {
 		name string
+		root *TreeNode
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "first", root: root, want: 275},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.SumNumbers(root))
 		})
 	}
 
