949ebe319b6849f1b082ae8e82fe4850344540ff
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-13 23:02:49 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package main
 
+import "math"
+
 type Solution struct{}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (s Solution) IsValidBST(root *TreeNode) bool {
+	return dfs(root, math.MinInt, math.MaxInt)
+}
+
+func dfs(root *TreeNode, min, max int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val <= min || root.Val >= max {
+		return false
+	}
+
+	return dfs(root.Left, min, root.Val) && dfs(root.Right, root.Val, max)
+}
--- main_test.go
+++ main_test.go
@@ -9,16 +9,46 @@ import (
 func TestSolution(t *testing.T) {
 	s := Solution{}
 
+	root1 := &TreeNode{
+		Val:   2,
+		Left:  &TreeNode{Val: 1},
+		Right: &TreeNode{Val: 3},
+	}
+
+	root2 := &TreeNode{
+		Val:  5,
+		Left: &TreeNode{Val: 1},
+		Right: &TreeNode{
+			Val:   7,
+			Left:  &TreeNode{Val: 4},
+			Right: &TreeNode{Val: 98},
+		},
+	}
+
+	root3 := &TreeNode{
+		Val:  1,
+		Left: &TreeNode{Val: 1},
+	}
+
+	root4 := &TreeNode{
+		Val:  0,
+		Left: &TreeNode{Val: -1},
+	}
+
 	testCases := []struct {
 		name string
-		want int
+		root *TreeNode
+		want bool
 	}{
-		{name: "test", want: 0},
+		{name: "first", root: root1, want: true},
+		{name: "second", root: root2, want: false},
+		{name: "third", root: root3, want: false},
+		{name: "fourth", root: root4, want: true},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.IsValidBST(tt.root))
 		})
 	}
 
