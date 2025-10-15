3fc7a8f7fb8965646fabf3065a61c26568ce58f8
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-15 20:48:35 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,44 @@
 package main
 
 type Solution struct{}
+
+// FindJudge map based solution:
+// T: O(n + len(trues)
+// S: O(n)
+// FindJudge
+//func (_ Solution) FindJudge(n int, trust [][]int) int {
+//	var (
+//		from = make(map[int]int)
+//		to   = make(map[int]int)
+//	)
+//
+//	for i := range trust {
+//		from[trust[i][0]]++
+//		to[trust[i][1]]++
+//	}
+//
+//	for i := 1; i < n+1; i++ {
+//		if from[i] == 0 && to[i] == n-1 {
+//			return i
+//		}
+//	}
+//
+//	return -1
+//}
+
+func (_ Solution) FindJudge(n int, trust [][]int) int {
+	var count = make([]int, n+1)
+
+	for i := range trust {
+		count[trust[i][0]]--
+		count[trust[i][1]]++
+	}
+
+	for i := 1; i < n+1; i++ {
+		if count[i] == n-1 {
+			return i
+		}
+	}
+
+	return -1
+}
--- main_test.go
+++ main_test.go
@@ -10,15 +10,18 @@ func TestSolution(t *testing.T) {
 	s := Solution{}
 
 	testCases := []struct {
-		name string
-		want int
+		name  string
+		trust [][]int
+		n     int
+		want  int
 	}{
-		{name: "test", want: 0},
+		{name: "first", n: 2, trust: [][]int{{1, 2}}, want: 2},
+		{name: "second", n: 1, trust: [][]int{}, want: 1},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.FindJudge(tt.n, tt.trust))
 		})
 	}
 
