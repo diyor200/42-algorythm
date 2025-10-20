e7ed9fc03d0742254c5752e78e6ca64d1c19656a
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-20 23:19:41 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
 type Solution struct{}
+
+// Brian Kernighan algorythm
+func (_ Solution) HammingDistance(x int, y int) int {
+	count := 0
+	n := x ^ y
+
+	for n != 0 {
+		count++
+		n &= n - 1
+	}
+
+	return count
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,16 @@ func TestSolution(t *testing.T) {
 
 	testCases := []struct {
 		name string
+		x, y int
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "first", x: 1, y: 4, want: 2},
+		{name: "second", x: 3, y: 1, want: 1},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.HammingDistance(tt.x, tt.y))
 		})
 	}
 
