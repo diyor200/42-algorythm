3fbd7c80e76fe246fffef2fdbaa312b4ab542603
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-20 22:47:26 +0500

    not solved

--- main.go
+++ main.go
@@ -1,3 +1,94 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	b := byte(48)
+	fmt.Println(int(b))
+	fmt.Println(tenPow(-1))
+	s := Solution{}
+	fmt.Println(s.MyAtoi("9223372036854775808"))
+}
+
 type Solution struct{}
+
+func (_ Solution) MyAtoi(s string) int {
+	zero := byte(48)
+	minus := byte(45)
+	negative := false
+
+	nums := []int{}
+	startIndex := 0
+
+	for i := range s {
+		if s[i] == ' ' || s[i] == '-' || s[i] == '+' {
+			if len(nums) != 0 {
+				break
+			}
+
+			if i > 0 && (s[i-1] == '-' || s[i-1] == '+') {
+				return 0
+			}
+
+			continue
+		} else if s[i]-zero < 0 || s[i]-zero > 9 {
+			if len(nums) >= 0 {
+				break
+			}
+		}
+
+		if s[i]-zero > 9 && len(nums) == 0 {
+			return 0
+		}
+
+		if s[i]-zero >= 0 {
+			if len(nums) == 0 {
+				startIndex = i
+			}
+
+			nums = append(nums, int(s[i]-zero))
+		}
+	}
+
+	if startIndex != 0 {
+		if s[startIndex-1] == minus {
+			negative = true
+		}
+	}
+
+	var res int64
+	n := len(nums)
+	for i := range nums {
+		res += int64(nums[i] * tenPow(n-i-1))
+	}
+
+	if negative {
+		res = -res
+	} else if res < 0 {
+		if math.MinInt32 > (-1 * res) {
+			res = math.MaxInt32
+		}
+	}
+
+	if math.MaxInt32 < res {
+		res = math.MaxInt32
+	}
+
+	if math.MinInt32 > res {
+		res = math.MinInt32
+	}
+
+	return int(res)
+}
+
+func tenPow(n int) int {
+	res := 1
+	for i := 0; i < n; i++ {
+		res *= 10
+	}
+
+	return res
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,22 @@ func TestSolution(t *testing.T) {
 
 	testCases := []struct {
 		name string
+		word string
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "first", word: "42", want: 42},
+		{name: "second", word: "   -42", want: -42},
+		{name: "third", word: "4193 with words", want: 4193},
+		{name: "fourth", word: "-91283472332", want: -2147483648},
+		{name: "fifth", word: "1337c0d3", want: 1337},
+		{name: "sixth", word: "words and 987", want: 0},
+		{name: "seventh", word: "+-12", want: 0},
+		{name: "eights", word: "9223372036854775808", want: 2147483647},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, s.MyAtoi(tt.word))
 		})
 	}
 
