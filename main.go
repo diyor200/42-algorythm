10d29c00e156963a2f63a91bf93ff1fe227f0bdd
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-22 21:27:09 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
 type Solution struct{}
+
+func reverseBits(n int) int {
+	var res int
+
+	for i := 0; i < 32; i++ {
+		res = (res << 1) | (n & 1)
+		n = n >> 1
+	}
+
+	return res
+}
--- main_test.go
+++ main_test.go
@@ -7,18 +7,19 @@ import (
 )
 
 func TestSolution(t *testing.T) {
-	s := Solution{}
 
 	testCases := []struct {
 		name string
+		num  int
 		want int
 	}{
-		{name: "test", want: 0},
+		{name: "first", num: 43261596, want: 964176192},
+		{name: "second", num: 2147483644, want: 1073741822},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, s)
+			assert.Equal(t, tt.want, reverseBits(tt.num))
 		})
 	}
 
