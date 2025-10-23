d70b6a670a09ee7eac8fcf2a611febbee20beebd
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-23 23:33:02 +0500

    solution

--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
+import "fmt"
+
 type Solution struct{}
+
+// url: https://leetcode.com/problems/missing-number/?envType=problem-list-v2&envId=bit-manipulation
+
+func missingNumber(nums []int) int {
+	n := len(nums)
+	fmt.Println(n)
+	s := (1 + n) * n / 2 // arithmetic progression
+	for i := range nums {
+		s -= nums[i]
+		fmt.Println(s)
+	}
+
+	return s
+}
+
+func missingNumber(nums []int) int {
+	xorNums := 0
+	for i := range nums {
+		xorNums ^= nums[i] ^ (i + 1)
+	}
+
+	return xorNums
+}
+
+func main() {
+	fmt.Println(missingNumber([]int{0, 1, 3}))
+}
