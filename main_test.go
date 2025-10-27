dc6a588d009b13efa94867279940c394f5af261e
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-10-27 20:51:20 +0500

    main

--- main_test.go
+++ main_test.go
@@ -14,13 +14,11 @@ func TestSolution(t *testing.T) {
 		want []int
 	}{
 		{name: "first", nums: []int{1, 2, 2, 4}, want: []int{2, 3}},
-		{name: "second", nums: []int{1, 1}, want: []int{1, 2}},
-		{name: "third", nums: []int{2, 2}, want: []int{2, 1}},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, findErrorNums(tt.nums))
+			assert.Equal(t, tt.want, tt.)
 		})
 	}
 
