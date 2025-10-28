f6d6e4c7d4eeb182efbec19c5b8b6781a06acedc
diyorbek <diyorbekabdulaxatov40@gmail.com>
2025-10-28 17:15:16 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
 package main
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	for i := 0; k > 0 && i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				return true
+			}
+			if k == (j - i) {
+				break
+			}
+		}
+	}
+
+	return false
+}
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	if k == 0 {
+		return false
+	}
+
+	n := len(nums)
+	if k > n {
+		k = n
+	}
+
+	seen := make(map[int]bool, k)
+	for i := 0; i < k; i++ {
+		if _, ok := seen[nums[i]]; ok {
+			return true
+		}
+		seen[nums[i]] = true
+	}
+
+	for i := k; i < len(nums); i++ {
+		if _, ok := seen[nums[i]]; ok {
+			return true
+		}
+		delete(seen, nums[i-k])
+		seen[nums[i]] = true
+	}
+
+	return false
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,22 @@ func TestSolution(t *testing.T) {
 	testCases := []struct {
 		name string
 		nums []int
-		want []int
+		k    int
+		want bool
 	}{
-		{name: "first", nums: []int{1, 2, 2, 4}, want: []int{2, 3}},
+		{name: "first", nums: []int{1, 2, 3, 1}, k: 3, want: true},
+		{name: "second", nums: []int{1, 0, 1, 1}, k: 1, want: true},
+		{name: "third", nums: []int{1, 2, 3, 1, 2, 3}, k: 2, want: false},
+		{name: "fourth", nums: []int{1, 2, 1}, k: 0, want: false},
+		{name: "fifth", nums: []int{1}, k: 1, want: false},
+		{name: "sixth", nums: []int{99, 99}, k: 2, want: true},
+		{name: "seventh", nums: []int{99, 99}, k: 3, want: true},
+		{name: "eights", nums: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, k: 15, want: false},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, tt.)
+			assert.Equal(t, tt.want, containsNearbyDuplicate(tt.nums, tt.k))
 		})
 	}
 
