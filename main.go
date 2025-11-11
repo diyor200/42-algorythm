a82ae2f6dc11bf68e2d5d62546dbf39b5eac40e4
diyorbek <diyorbekabdulaxatov40@gmail.com>
2025-11-11 10:39:49 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+func longestPalindrome(s string) int {
+	if len(s) == 1 {
+		return 1
+	}
+
+	letters := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		letters[s[i]]++
+	}
+
+	res := 0
+	for k := range letters {
+		if letters[k] > 1 {
+			res += letters[k] / 2
+			delete(letters, k)
+		}
+	}
+
+	res *= 2
+	if len(letters) > 0 {
+		res += 1
+	}
+
+	return res
+}
--- main_test.go
+++ main_test.go
@@ -10,15 +10,16 @@ func TestSolution(t *testing.T) {
 
 	testCases := []struct {
 		name string
-		nums []int
-		want []int
+		s    string
+		want int
 	}{
-		{name: "first", nums: []int{1, 2, 2, 4}, want: []int{2, 3}},
+		{name: "first", s: "abccccdd", want: 7},
+		{name: "second", s: "a", want: 1},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, tt.)
+			assert.Equal(t, tt.want, longestPalindrome(tt.s))
 		})
 	}
 
