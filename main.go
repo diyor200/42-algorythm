b80e76dac773c1953c62f9134a5ce52e670d1359
diyorbek <diyorbekabdulaxatov40@gmail.com>
2025-11-25 13:58:32 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+func flipAndInvertImage(image [][]int) [][]int {
+	for img := range image {
+		reverse(image[img])
+		for i := range image[img] {
+			image[img][i] ^= 1
+		}
+	}
+
+	return image
+}
+
+func reverse(nums []int) {
+	n := len(nums) - 1
+	for i := 0; i < n; i++ {
+		nums[i], nums[n] = nums[n], nums[i]
+		n--
+	}
+}
--- main_test.go
+++ main_test.go
@@ -9,16 +9,25 @@ import (
 func TestSolution(t *testing.T) {
 
 	testCases := []struct {
-		name string
-		nums []int
-		want []int
+		name  string
+		image [][]int
+		want  [][]int
 	}{
-		{name: "first", nums: []int{1, 2, 2, 4}, want: []int{2, 3}},
+		{
+			name:  "first",
+			image: [][]int{{1, 1, 0}, {1, 0, 1}, {0, 0, 0}},
+			want:  [][]int{{1, 0, 0}, {0, 1, 0}, {1, 1, 1}},
+		},
+		{
+			name:  "second",
+			image: [][]int{{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}},
+			want:  [][]int{{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 1}, {1, 0, 1, 0}},
+		},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, tt.)
+			assert.Equal(t, tt.want, flipAndInvertImage(tt.image))
 		})
 	}
 
