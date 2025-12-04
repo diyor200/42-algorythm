0bf40da6a1427daa99d37ef0c75c7fbdff75da72
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-12-04 22:30:37 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,76 @@
 package main
+
+import "fmt"
+
+func main() {
+	list := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+					},
+				},
+			},
+		},
+	}
+	//list = &ListNode{Val: 1}
+
+	head := rotateRight(list, 99)
+	read(head)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	l := length(head)
+	k = k % l
+
+	if k == 0 {
+		return head
+	}
+
+	node := head
+	for i := 0; i < l-k-1; i++ {
+		node = node.Next
+	}
+
+	newHead := node.Next
+	tail := newHead
+	for tail.Next != nil {
+		tail = tail.Next
+	}
+
+	tail.Next = head
+	node.Next = nil
+	return newHead
+}
+
+func length(node *ListNode) int {
+	res := 0
+
+	for node != nil {
+		res++
+		node = node.Next
+	}
+
+	return res
+}
+
+func read(head *ListNode) {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
--- main_test.go
+++ main_test.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"testing"
-
-	"github.com/go-playground/assert/v2"
 )
 
 func TestSolution(t *testing.T) {
-
-	testCases := []struct {
-		name string
-		nums []int
-		want []int
-	}{
-		{name: "first", nums: []int{1, 2, 2, 4}, want: []int{2, 3}},
-	}
-
-	for _, tt := range testCases {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, tt.)
-		})
-	}
 
 	t.Log("pass")
 }
