478de58e1bad165029aa5b24746fdac514e20f78
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-12-04 21:43:58 +0500

    solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ```42.uz algorythm course```
+
+problem url: ```https://leetcode.com/problems/swap-nodes-in-pairs/description/?envType=problem-list-v2&envId=linked-list```
--- main.go
+++ main.go
@@ -1,1 +1,51 @@
 package main
+
+import "fmt"
+
+func main() {
+	head := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+				},
+			},
+		},
+	}
+
+	head = swapPairs(head)
+	read(head)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	prev := dummy
+
+	for head != nil && head.Next != nil {
+		first, second := head, head.Next
+
+		prev.Next = second
+		first.Next = second.Next
+		second.Next = first
+
+		prev = first
+		head = first.Next
+	}
+
+	return dummy.Next
+}
+
+func read(head *ListNode) {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
