0169e720fbf64563fed9a9f0dd2e5fa232dd1a6d
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-12-11 23:01:45 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import "fmt"
+
+func guessNumber(n int) int {
+	middle := int(n / 2)
+	res := guess(middle)
+	left, right := 0, n
+	for res != 0 {
+		switch res {
+		case 1:
+			left = middle + 1
+		case -1:
+			right = middle
+		case 0:
+			break
+		}
+
+		middle = (left + right) / 2
+		res = guess(middle)
+	}
+
+	return middle
+}
+
+func main() {
+	n := 30
+
+	fmt.Println(guessNumber(n))
+}
+
+func guess(x int) int {
+	guessed := 7
+	if x > guessed {
+		return -1
+	} else if x < guessed {
+		return 1
+	}
+
+	return 0
+}
