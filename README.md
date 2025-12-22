9f4e8d8337e942fef9f1d8b8e7069e80b583e763
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-12-22 22:14:36 +0500

    solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ```42.uz algorythm course```
+
+url: https://leetcode.com/problems/print-foobar-alternately/?envType=problem-list-v2&envId=concurrency
--- main.go
+++ main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type FooBar struct {
+	n          int
+	fooChannel chan struct{}
+	barChannel chan struct{}
+}
+
+func NewFooBar(n int) *FooBar {
+	fooChannel := make(chan struct{}, 1)
+	barChannel := make(chan struct{}, 1)
+	return &FooBar{n: n, fooChannel: fooChannel, barChannel: barChannel}
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+	for i := 0; i < fb.n; i++ {
+		<-fb.fooChannel
+		// printFoo() outputs "foo". Do not change or remove this line.
+		printFoo()
+		fb.barChannel <- struct{}{}
+	}
+}
+
+func (fb *FooBar) Bar(printBar func()) {
+	fb.fooChannel <- struct{}{}
+	for i := 0; i < fb.n; i++ {
+		// printBar() outputs "bar". Do not change or remove this line.
+		<-fb.barChannel
+		printBar()
+		fb.fooChannel <- struct{}{}
+	}
+}
+
+func main() {
+	fb := NewFooBar(2)
+	go fb.Foo(printfoo)
+	go fb.Bar(printbar)
+
+	time.Sleep(time.Second)
+}
+
+func printfoo() {
+	fmt.Print("foo")
+}
+
+func printbar() {
+	fmt.Print("bar")
+}
