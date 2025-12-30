7d136ea4182930af9adc1b4e38b54d6edd04a81c
diyorbek <diyorbekabdulaxatov40@gmail.com>
2025-12-30 10:06:58 +0500

    solution

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+// url: https://leetcode.com/problems/find-pivot-index/description/?envType=problem-list-v2&envId=prefix-sum
+
+func pivotIndex(nums []int) int {
+	rightSum, leftSum := 0, 0
+	for i := range nums {
+		rightSum += nums[i]
+	}
+
+	for i := range nums {
+		if leftSum == rightSum-nums[i] {
+			return i
+		}
+		leftSum += nums[i]
+		rightSum -= nums[i]
+	}
+
+	return -1
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,16 @@ func TestSolution(t *testing.T) {
 	testCases := []struct {
 		name string
 		nums []int
-		want []int
+		want int
 	}{
-		{name: "first", nums: []int{1, 2, 2, 4}, want: []int{2, 3}},
+		{name: "first", nums: []int{1, 7, 3, 6, 5, 6}, want: 3},
+		{name: "second", nums: []int{1, 2, 3}, want: -1},
+		{name: "third", nums: []int{2, 1, -1}, want: 0},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, tt.)
+			assert.Equal(t, tt.want, pivotIndex(tt.nums))
 		})
 	}
 
